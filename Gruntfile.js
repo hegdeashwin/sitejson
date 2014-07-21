60918f1fb3a21cf95d5de1f98009b8fc2b723092
hegdeashwin <ashwin.hegde3@gmail.com>
2014-07-22 00:33:15 +0530

    Adds jsonlint Tasks
    Signed-off-by: hegdeashwin <ashwin.hegde3@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,15 @@ module.exports = function(grunt) {
     "use strict";
 
     grunt.initConfig({
+        jsonlint: {
+            files: {
+                src: [
+                    'package.json',
+                    'config/.jscsrc',
+                    'config/.jshintrc'
+                ]
+            }
+        },
         jshint: {
             all: ["lib/**/*.js", "bin/**/*.js"],
             options: {
@@ -22,11 +31,13 @@ module.exports = function(grunt) {
      */
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-jscs-checker");
+    grunt.loadNpmTasks('grunt-jsonlint');
 
     /**
      * Define tasks : Tasks for development eco - system.
      */
     grunt.registerTask("default", [
+        "jsonlint",
         "jshint",
         "jscs"
     ]);
